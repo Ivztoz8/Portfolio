d808f3d38202070d93c2eea9f210f559218b85f4
Ivan Toselli <ivan.toselli31@gmail.com>
2023-03-03 18:52:10 +0100

    Mobile banner ADD

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
+M.AutoInit();
 
+
+
+// Close button
+window.onload = function(){
+    document.getElementsByClassName('close').onclick = function(){
+        this.parentNode.parentNode.remove();
+        return false;
+    };
+};
